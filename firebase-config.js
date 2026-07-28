ad135feea11f59f6d49451357294257809c602d0
hukdda <gc4res@gmail.com>
2026-07-28 16:15:16 +0900

    Add Firebase configuration to firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,2 +1,8 @@
-// Firebase 콘솔의 웹 앱 설정값으로 교체됩니다.
-window.GVOR_FIREBASE_CONFIG = null;
+window.GVOR_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBxiaqns8kdjKB6uh-QTXn5B3K54kP38hA",
+  authDomain: "global-video-opportunity-radar.firebaseapp.com",
+  projectId: "global-video-opportunity-radar",
+  storageBucket: "global-video-opportunity-radar.firebasestorage.app",
+  messagingSenderId: "975459606829",
+  appId: "1:975459606829:web:99a35643a5d99ba3a1f3a4"
+};
