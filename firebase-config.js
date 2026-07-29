298e726ec892ad199bb5a5e74a93773321fe9642
hukdda <gc4res@gmail.com>
2026-07-29 20:07:27 +0900

    Change authDomain to use web.app
    Updated authDomain to use web.app instead of firebaseapp.com.

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 // Firebase 콘솔의 웹 앱 설정값으로 교체됩니다.
 window.GVOR_FIREBASE_CONFIG = {
   apiKey: "AIzaSyBxiaqns8kdjKB6uh-QTXn5B3K54kP38hA",
-  authDomain: "global-video-opportunity-radar.firebaseapp.com",
+  authDomain: "global-video-opportunity-radar.web.app",
   projectId: "global-video-opportunity-radar",
   storageBucket: "global-video-opportunity-radar.firebasestorage.app",
   messagingSenderId: "975459606829",
